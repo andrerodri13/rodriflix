bf695ad0a0a97f641656be9db04e7ac1a8afd3d5
André S. Rodrigues <andre@one7.com.vc>
2020-07-28 21:34:31 -0300

    Dia 2 imersao react

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Logo from '../../assets/imagens/Logo_roxo.png';
 import './Menu.css';
 // import ButtonLink from '../ButtonLink';
@@ -11,7 +12,7 @@ function Menu() {
         <img src={Logo} className="Logo" alt="Rodriflix logo"/>
       </a>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
 
